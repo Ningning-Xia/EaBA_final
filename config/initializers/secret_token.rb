2b33a28273ce7f3dff6dd15002b6b3a434a26c4b
Wei Wang <chengtian.wang@gmail.com>
2012-11-12 10:54:43 -0500

    Added all initial files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EaBA::Application.config.secret_token = '908218b1c1432547c5aadcea888592474f24aaa3f23d7beba3f39718bf28b8e1ceae8a807c6af30516c541e5daf8e6a6c05123e2e26cce183b9ac9368f76ca2e'
